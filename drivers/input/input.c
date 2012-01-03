f73d11f011091af4b444541796786c014759a2f5
JoeyJiao <joey.jiaojg@gmail.com>
2012-01-03 20:37:58 +0800

    Not add blank name input device

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -1540,6 +1540,12 @@ int input_register_device(struct input_dev *dev)
 		dev->name ? dev->name : "Unspecified device", path ? path : "N/A");
 	kfree(path);
 
+	if(!dev->name){
+		printk(KERN_DEBUG "Not specified input device name.\n");
+		device_del(&dev->dev);
+		return -1;
+	}
+
 	error = mutex_lock_interruptible(&input_mutex);
 	if (error) {
 		device_del(&dev->dev);
