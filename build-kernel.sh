406bfe6201e8f7f29fe0769d03a715156944d07a
JoeyJiao <joey.jiaojg@gmail.com>
2012-03-08 09:00:08 +0800

    Fix wifi unload module results in kernel panic/die by enable CONFIG_KALLSYMS; Disable input mouse config;

--- build-kernel.sh
+++ build-kernel.sh
@@ -2,4 +2,4 @@
 #export CC='ccache gcc'
 #ARCH=arm CROSS_COMPILE='ccache /home/joeyjiaojg/android/src/cm2.3.7/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-' make zImage -j8
 #ARCH=arm CROSS_COMPILE='/home/joeyjiaojg/android/src/cm2.3.7/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-' make zImage -j4
-ARCH=arm CROSS_COMPILE='ccache /home/joeyjiaojg/android/src/arm-none-eabi-gcc-4_6/bin/arm-none-eabi-' make zImage -j4
+ARCH=arm CROSS_COMPILE='ccache /home/joeyjiaojg/android/src/arm-none-eabi-gcc-4_6/bin/arm-none-eabi-' make zImage -j4 2>~/Desktop/build-kernel.txt
